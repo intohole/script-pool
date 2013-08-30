b8bbb50a2d776716c22d2a700f3c81e4194b2bd1
lixuze <lixuze@iyuewe.com>
2013-08-30 14:00:29 +0800

    添加杀死进程 之后的状态 0 添加避免杀死脚本命令  grep -V bash

--- sh/kill_all.sh
+++ sh/kill_all.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 #循环所有列表中 查看qq进程 不包含有 grep使用的进程 进程号在第二列 awk 执行  
-for i in $(ps aux | grep qq | grep -v "grep" | awk {'print $2'});do kill -9 $i; done
+if [ $# -lt 1 ];then
+	echo "sh kill_all [thread name]"
+fi
+
+for i in $(ps aux | grep $1 | grep -v "grep" | grep -v $0 | awk {'print $2'});do echo "kill"$i ;kill -9 $i; echo "kill"$i" state"$?; done
