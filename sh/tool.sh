f808fd25e44c099ceca11b903a12451674f50d9a
86 <intoblack86@gmail.com>
2013-09-06 14:06:55 +0800

    Update tool.sh

--- sh/tool.sh
+++ sh/tool.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 
-
+#得到文件行数
 get_file_line()
 {
 	_sum=$(wc -l $1 | tail -n 1 | awk '{print $1}')
 	return $_sum
 }
 
-
+#删除没有行数的空文件
 rm_zero_line()
 {
 	for file in $( wc -l | awk '{(if($1==0))print$2}');do
